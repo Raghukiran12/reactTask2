d4fa4006365c6f02bbeed0a3a9d0194ee2e42a6a
Raghu Kiran <Raghu@raghukiran188@gmail.com>
2023-10-13 14:58:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from "./Components_2/Calculator";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
